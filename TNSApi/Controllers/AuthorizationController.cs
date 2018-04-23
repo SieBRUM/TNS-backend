a926cf00c4c6cb41ba4db80a2e8a47387c1004d0
SieBRUM <siebrenkraak@hotmail.com>
2018-04-23 09:41:40 +0200

    Removed usings.

--- TNSApi/Controllers/AuthorizationController.cs
+++ TNSApi/Controllers/AuthorizationController.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
 using TNSApi.Mapping;
-using TNSApi.Models;
 using TNSApi.Services;
 
 namespace TNSApi.Controllers
